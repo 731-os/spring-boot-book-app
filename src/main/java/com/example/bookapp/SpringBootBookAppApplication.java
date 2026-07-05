26bbb87a75448cd88c8ccd98a0153cd51318463e
731-os <187982573+731-os@users.noreply.github.com>
2026-07-05 17:41:17 +0900

    Spring Bootプロジェクトの初期構成

---
+++ src/main/java/com/example/bookapp/SpringBootBookAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBookAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBookAppApplication.class, args);
+    }
+
+}
